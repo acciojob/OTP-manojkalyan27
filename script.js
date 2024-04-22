2a04791a43252acdf8f6da8754359d637c5ead1b
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-04-22 06:02:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const inps = document.querySelectorAll('.code');
+inps[0].focus();
+
+inps.forEach((inp , index) => {
+    inp.addEventListener("input",() => {
+        if(inp.value.length === 1 && index < inps.length -1){
+            inps[index+1].focus();
+        }
+
+    })
+
+
+    inp.addEventListener("keydown", (event) =>{
+        if(event.key === "Backspace" && index > 0) {
+            inps[index].value = '';
+            inps[index-1].focus();
+        }
+    })
+});
